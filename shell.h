fdb5756d974d9db512a731459aef731bedbe0cde
alex <70367958+lolalex1@users.noreply.github.com>
2025-05-05 09:39:56 +1000

    Forgot some things in the header file.

--- shell.h
+++ shell.h
@@ -13,6 +13,8 @@ void display_prompt(void);
 char *read_line(void);
 char *strip_newline(char *line);
 int execute_command(char **args);
-
 char *find_command_in_path(char *command);
+char **tokenize_line(char *line);
+void free_tokens(char **tokens);
+
 #endif
