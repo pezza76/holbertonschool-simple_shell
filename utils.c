295b0ef6fd038c8936c7556e4d532cfa6f7e86eb
alex <70367958+lolalex1@users.noreply.github.com>
2025-05-08 11:03:29 +1000

    More betty

--- utils.c
+++ utils.c
@@ -23,8 +23,9 @@ char *read_line(void)
 char *strip_newline(char *line)
 {
 	size_t len = strlen(line);
+
 	if (len > 0 && line[len - 1] == '\n')
-			line[len - 1] = '\0';
+		line[len - 1] = '\0';
 	return (line);
 }
 
