56e958782e5fc225026ff394408982b93aaf4851
Andrew Perry <pezza76@hotmail.com>
2025-05-08 01:52:31 +0000

    task4

--- main.c
+++ main.c
@@ -4,55 +4,63 @@ int main(void)
 {
 	char *line, *clean_line;
 	char **args = NULL;
+	int last_status = 0;
 
 	while (1)
 	{
+	if (isatty(STDIN_FILENO))
+		display_prompt();
+
+	line = read_line();
+
+	if (!line)
+	{
 		if (isatty(STDIN_FILENO))
-			display_prompt();
-
-		line = read_line();
-
-		if (!line)
 		{
-			if (isatty(STDIN_FILENO))
-			{
-				write(STDOUT_FILENO, "\n", 1);
-			}
-			break;
+			write(STDOUT_FILENO, "\n", 1);
 		}
-
-		clean_line = strip_newline(line);
-		if (*clean_line != '\0')
+		if (!isatty(STDIN_FILENO))
 		{
-			args = tokenize_line(clean_line);
-			if (args != NULL)
-			{
-				if (strcmp(args[0], "exit") == 0)
-				{
-					free_tokens(args);
-					free(line);
-					break;
-				}
-
-				if (strcmp(args[0], "env") == 0)
-				{
-					int i = 0;
-					while (environ[i])
-					{
-						write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-						write(STDOUT_FILENO, "\n", 1);
-						i++;
-					}
-					free_tokens(args);
-					continue;
-				}
-
-				execute_command(args);
-				free_tokens(args);
-			}
+			exit(last_status);
 		}
-
-		free(line);
+		break;
 	}
-	return (0);
+
+	clean_line = strip_newline(line);
+
+	if (*clean_line != '\0')
+	{
+		args = tokenize_line(clean_line);
+
+		if (args != NULL)
+		{
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free_tokens(args);
+			free(line);
+			exit(last_status);
+		}
+
+		if (strcmp(args[0], "env") == 0)
+		{
+			int i = 0;
+			while (environ[i])
+			{
+				write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+				write(STDOUT_FILENO, "\n", 1);
+				i++;
+			}
+
+			free_tokens(args);
+			continue;
+		}
+
+		last_status = execute_command(args);
+		free_tokens(args);
+		}
+	}
+
+	free(line);
+	
+	return last_status;
 }
