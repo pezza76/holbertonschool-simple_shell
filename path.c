8c194b5ae7d588035194029b58547b612df214a0
pezza76 <pezza76@hotmail.com>
2025-08-20 16:28:21 +1000

    Update path.c

--- path.c
+++ path.c
@@ -2,27 +2,47 @@
 #include <sys/stat.h>
 
 /**
- * find_command_in_path - Searches for a command in the system PATH
- * @command: Command name to search for
+ * search_in_dirs - Searches for a command in a list of directories.
+ * @dir: The first directory from the tokenized PATH.
+ * @command: The command to search for.
  *
- * Return: Full path string if found and executable, NULL otherwise
+ * Return: The full path of the command if found, otherwise NULL.
+ */
+static char *search_in_dirs(char *dir, char *command)
+{
+	char *full_path;
+	struct stat st;
+
+	while (dir)
+	{
+		full_path = malloc(strlen(dir) + strlen(command) + 2);
+		if (!full_path)
+			return (NULL);
+
+		sprintf(full_path, "%s/%s", dir, command);
+
+		/* Check if the file exists and is executable */
+		if (stat(full_path, &st) == 0 && access(full_path, X_OK) == 0)
+			return (full_path);
+
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+	return (NULL);
+}
+
+/**
+ * find_command_in_path - Searches for a command in the system PATH.
+ * @command: Command name to search for.
+ *
+ * Return: Full path string if found and executable, NULL otherwise.
  */
 char *find_command_in_path(char *command)
 {
-	char *path = NULL, *path_copy, *dir, *full_path;
-	struct stat st;
-	int i = 0;
+	char *path, *path_copy, *dir, *full_path = NULL;
 
-	while (environ[i])
-	{
-		if (strncmp(environ[i], "PATH=", 5) == 0)
-		{
-			path = environ[i] + 5;
-			break;
-		}
-		i++;
-	}
-
+	/* Use getenv for a safer and simpler way to get PATH */
+	path = getenv("PATH");
 	if (!path)
 		return (NULL);
 
@@ -31,27 +51,8 @@ char *find_command_in_path(char *command)
 		return (NULL);
 
 	dir = strtok(path_copy, ":");
-	while (dir)
-	{
-		full_path = malloc(strlen(dir) + strlen(command) + 2);
-		if (!full_path)
-		{
-			free(path_copy);
-			return (NULL);
-		}
-
-		sprintf(full_path, "%s/%s", dir, command);
-
-		if (stat(full_path, &st) == 0 && access(full_path, X_OK) == 0)
-		{
-			free(path_copy);
-			return (full_path);
-		}
-
-		free(full_path);
-		dir = strtok(NULL, ":");
-	}
+	full_path = search_in_dirs(dir, command);
 
 	free(path_copy);
-	return (NULL);
+	return (full_path);
 }
